922d81684595df912505775c4454486b82f7c7f4
Jonah <jnorton@pipeline.sbcc.edu>
2025-06-12 20:51:18 -0700

    Patch: Add any[] type to providers array to fix Web3 build error

--- src/context/Web3.tsx
+++ src/context/Web3.tsx
@@ -1,7 +1,12 @@
 import { ethers } from "ethers";
 
 export function connect() {
-  let providers = [];
+  /* Without casting, led to type inference issue. 'providers' implicitly has type 'any[]' in some locations where the type can't be determined.
+  */
+  // let providers = [];
+
+  // In future, we can  also define an interface for providers later. But for now, a safe patch is:
+  let providers: any[] = [];
 
   window.addEventListener("eip6963:announceProvider", (event) => {
     providers.push(event.detail);
